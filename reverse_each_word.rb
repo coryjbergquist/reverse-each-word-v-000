c02bfadaa48a5df89c78ae0e86ab686e283f3085
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-17 17:25:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,31 +1,11 @@
-def reverse_each_word(str)
-rb_str_reverse_bang(VALUE str)
-{
-    if (RSTRING_LEN(str) > 1) {
-        if (single_byte_optimizable(str)) {
-            char *s, *e, c;
-
-            str_modify_keep_cr(str);
-            s = RSTRING_PTR(str);
-            e = RSTRING_END(str) - 1;
-            while (s < e) {
-                c = *s;
-                *s++ = *e;
-                *e-- = c;
-            }
-        }
-        else {
-            str_shared_replace(str, rb_str_reverse(str));
-        }
-    }
-    else {
-        str_modify_keep_cr(str);
-    }
-    return str;
-}
+def reverse_each_word(string)
+array = string.split" "
+new_array = []
+array.each {|x| new_array << x.reverse}
+new_array.join" "
 end
 
 def new_reverse_each_word(string)
   array = string.split" "
-  array.collect.reverse
+  array.collect
 end
