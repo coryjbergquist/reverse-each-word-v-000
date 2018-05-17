8339807f71b8db480157642aeebe5f5b31892571
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-17 17:31:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,5 @@
 def reverse_each_word(string)
 array = string.split" "
-new_array = []
-array.each {|x| new_array << x.reverse}
-new_array.join" "
-end
-
-def new_reverse_each_word(string)
-  array = string.split" "
  new_array = [] 
  array.collect {|x| new_array << x.reverse}
  new_array
